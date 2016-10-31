ac62c3d2ab88beb11b6730af7c8274bafec16a74
Chris Foster <cdbfoster@gmail.com>
2016-10-31 16:08:04 -0400

    Fix the license header in state.rs

--- src/impls/tak/state/state.rs
+++ src/impls/tak/state/state.rs
@@ -1,18 +1,18 @@
 //
-// This file is part of zero_sum-tak.
+// This file is part of zero_sum.
 //
-// zero_sum-tak is free software: you can redistribute it and/or modify
+// zero_sum is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// zero_sum-tak is distributed in the hope that it will be useful,
+// zero_sum is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with zero_sum-tak. If not, see <http://www.gnu.org/licenses/>.
+// along with zero_sum. If not, see <http://www.gnu.org/licenses/>.
 //
 // Copyright 2016 Chris Foster
 //
