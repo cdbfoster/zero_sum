f2d8ecefde28b800f5fea160185961284d14ee07
Chris Foster <cdbfoster@gmail.com>
2017-11-28 16:10:46 -0500

    Make CompositeLayer copyless.

--- smallann/src/layer/composite.rs
+++ smallann/src/layer/composite.rs
@@ -17,10 +17,9 @@
 // Copyright 2017 Chris Foster
 //
 
-use std::cell::RefCell;
 use std::mem;
 
-use smallmath::matrix::{self, Matrix};
+use smallmath::matrix::Matrix;
 
 use layer::Layer;
 
@@ -29,35 +28,22 @@ pub struct CompositeLayer {
     inputs: usize,
     outputs: usize,
     pub(crate) layers: Vec<Box<Layer>>,
-
-    input_buffers: RefCell<Vec<Matrix>>,
-    output_buffers: RefCell<Vec<Matrix>>,
-    previous_gradient_buffers: RefCell<Vec<Matrix>>,
 }
 
 impl CompositeLayer {
     pub fn new(layers: Vec<Box<Layer>>) -> CompositeLayer {
         let mut inputs = 0;
         let mut outputs = 0;
-        let mut input_buffers = Vec::new();
-        let mut output_buffers = Vec::new();
-        let mut previous_gradient_buffers = Vec::new();
 
         for layer in &layers {
             inputs += layer.inputs();
             outputs += layer.outputs();
-            input_buffers.push(Matrix::zeros(10, layer.inputs()));
-            output_buffers.push(Matrix::zeros(10, layer.outputs()));
-            previous_gradient_buffers.push(Matrix::zeros(10, layer.inputs()));
         }
 
         CompositeLayer {
             inputs: inputs,
             outputs: outputs,
             layers: layers,
-            input_buffers: RefCell::new(input_buffers),
-            output_buffers: RefCell::new(output_buffers),
-            previous_gradient_buffers: RefCell::new(previous_gradient_buffers),
         }
     }
 }
@@ -72,87 +58,64 @@ impl Layer for CompositeLayer {
     }
 
     fn feed_forward(&self, inputs: &Matrix, outputs: &mut Matrix) {
-        let mut input_buffers = self.input_buffers.borrow_mut();
-        let mut output_buffers = self.output_buffers.borrow_mut();
-
+        // XXX Add debug asserts on the sizes of the matrices
         let mut input_position = 0;
         let mut output_position = 0;
 
         for i in 0..self.layers.len() {
-            let single_input = unsafe { Matrix::from_raw_parts(
-                1, self.layers[i].inputs(),
-                mem::transmute(inputs.as_ptr().offset(input_position as isize)),
+            let layer_inputs = unsafe { Matrix::from_raw_parts(
+                inputs.rows(), self.layers[i].inputs(),
+                mem::transmute(inputs.as_ptr().offset((input_position * inputs.rows()) as isize)),
                 0,
             ) };
-            let mut single_output = unsafe { Matrix::from_raw_parts(
-                1, self.layers[i].outputs(),
-                outputs.as_mut_ptr().offset(output_position as isize),
+            let mut layer_outputs = unsafe { Matrix::from_raw_parts(
+                inputs.rows(), self.layers[i].outputs(),
+                outputs.as_mut_ptr().offset((output_position * inputs.rows()) as isize),
                 0,
             ) };
 
-            {
-                let layer_inputs = if inputs.rows() == 1 {
-                    &single_input
-                } else {
-                    input_buffers[i].resize(inputs.rows(), self.layers[i].inputs());
-                    matrix::ops::clone_region(inputs, 0, input_position, &mut input_buffers[i], 0, 0, inputs.rows(), self.layers[i].inputs());
-                    &input_buffers[i]
-                };
-
-                let layer_outputs = if inputs.rows() == 1 {
-                    &mut single_output
-                } else {
-                    output_buffers[i].resize(inputs.rows(), self.layers[i].outputs());
-                    &mut output_buffers[i]
-                };
-
-                self.layers[i].feed_forward(layer_inputs, layer_outputs);
-
-                if inputs.rows() > 1 {
-                    matrix::ops::clone_region(layer_outputs, 0, 0, outputs, 0, output_position, inputs.rows(), self.layers[i].outputs());
-                }
-            }
+            self.layers[i].feed_forward(&layer_inputs, &mut layer_outputs);
 
             input_position += self.layers[i].inputs();
             output_position += self.layers[i].outputs();
 
-            mem::forget(single_input);
-            mem::forget(single_output);
+            mem::forget(layer_inputs);
+            mem::forget(layer_outputs);
         }
     }
 
     fn propagate_backward(&mut self, gradients: &Matrix, previous_inputs: &Matrix, previous_gradients: &mut Matrix, rate: f32) {
-        let mut input_buffers = self.input_buffers.borrow_mut();
-        let mut output_buffers = self.output_buffers.borrow_mut();
-        let mut previous_gradient_buffers = self.previous_gradient_buffers.borrow_mut();
-
+        // XXX Add debug asserts on the sizes of the matrices
         let mut input_position = 0;
         let mut output_position = 0;
 
         for i in 0..self.layers.len() {
-            let layer_gradients = {
-                output_buffers[i].resize(gradients.rows(), self.layers[i].outputs());
-                matrix::ops::clone_region(gradients, 0, output_position, &mut output_buffers[i], 0, 0, gradients.rows(), self.layers[i].outputs());
-                &output_buffers[i]
-            };
+           let layer_gradients = unsafe { Matrix::from_raw_parts(
+                gradients.rows(), self.layers[i].outputs(),
+                mem::transmute(gradients.as_ptr().offset((output_position * gradients.rows()) as isize)),
+                0,
+            ) };
 
-            let layer_previous_inputs = {
-                input_buffers[i].resize(gradients.rows(), self.layers[i].inputs());
-                matrix::ops::clone_region(previous_inputs, 0, input_position, &mut input_buffers[i], 0, 0, gradients.rows(), self.layers[i].inputs());
-                &input_buffers[i]
-            };
+            let layer_previous_inputs = unsafe { Matrix::from_raw_parts(
+                gradients.rows(), self.layers[i].inputs(),
+                mem::transmute(previous_inputs.as_ptr().offset((input_position * gradients.rows()) as isize)),
+                0,
+            ) };
 
-            let layer_previous_gradients = {
-                previous_gradient_buffers[i].resize(gradients.rows(), self.layers[i].inputs());
-                &mut previous_gradient_buffers[i]
-            };
+            let mut layer_previous_gradients = unsafe { Matrix::from_raw_parts(
+                gradients.rows(), self.layers[i].inputs(),
+                previous_gradients.as_mut_ptr().offset((input_position * gradients.rows()) as isize),
+                0,
+            ) };
 
-            self.layers[i].propagate_backward(layer_gradients, layer_previous_inputs, layer_previous_gradients, rate);
-
-            matrix::ops::clone_region(layer_previous_gradients, 0, 0, previous_gradients, 0, input_position, gradients.rows(), self.layers[i].inputs());
+            self.layers[i].propagate_backward(&layer_gradients, &layer_previous_inputs, &mut layer_previous_gradients, rate);
 
             input_position += self.layers[i].inputs();
             output_position += self.layers[i].outputs();
+
+            mem::forget(layer_gradients);
+            mem::forget(layer_previous_inputs);
+            mem::forget(layer_previous_gradients);
         }
     }
 
